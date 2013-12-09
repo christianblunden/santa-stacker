b913133405eb311ec06c4b94a75ce8ed2cd91aac
Nikita Beloglazov <nikelandjelo@gmail.com>
2013-12-09 21:57:16 +0000

    Add 3d visualization.

--- src/santa_stacker/scoring.clj
+++ src/santa_stacker/scoring.clj
@@ -39,10 +39,6 @@
                       (Math/abs (- (inc ind) id))))
        (apply +)))
 
-(score-order conf)
-
-(score-z conf)
-
 (defn score [conf]
   (+ (* 2 (score-z conf))
      (score-order conf)))
@@ -50,23 +46,55 @@
 (def sze 20)
 
 (defn setup []
-  (frame-rate 1)                    ;;Set framerate to 1 FPS
-  (background 200))                 ;;Set the background colour to
-                                    ;;  a nice shade of grey.
+;  (frame-rate 1)
+  (background 200))
+
+(def colors (repeatedly #(list (+ 128 (rand-int 127))
+                               (+ 128 (rand-int 127))
+                               (+ 128 (rand-int 127)))))
+
+(defn draw3d []
+  (push-matrix)
+  (background 200)
+;  (translate 1 1)
+  (translate (/ (width) 2) (/ (height) 2))
+  (scale 30)
+  (rotate-y (* (mouse-x) (/ 2.0 (width)) TWO-PI))
+  (rotate-x (* (mouse-y) (/ 2.0 (height)) TWO-PI))
+  (println "\n\n\n")
+  (doseq [[id cells] conf2]
+    (let [sizes (map - (take-last 3 cells) (take 3 cells) [-1 -1 -1])
+          [x1 y1 z1] (map + cells [-1 -1 -1]
+                          (map #(/ % 2) sizes))]
+      (fill (apply color (nth colors id)))
+      (with-translation [x1 y1 z1]
+        (let [sizes (map - (take-last 3 cells) (take 3 cells) [-1 -1 -1])]
+          (println :id id :coords x1 y1 z1 sizes)
+
+          (apply box sizes))
+        (fill 0)
+        (with-translation [0 5 0]
+          (text (str id) )))))
+  (pop-matrix))
+
 (defn draw []
   (doseq [[id cells] conf2]
     (let [x1 (first cells)
           z1 (- 10 (nth cells 2) )
           x2 (inc (nth cells 21))
           z2 (- 10 (inc (nth cells 23)))]
+      (fill (apply color (nth colors id)))
       (println z1 z2)
-      (rect (* sze x1) (* sze z1) (* sze (- x2 x1)) (* sze (- z2 z1))))))
+      (rect (* sze x1) (* sze z1) (* sze (- x2 x1)) (* sze (- z2 z1)))
+      (fill 0)
+      (text (str id) (* 1/2 (+ x1 x2) sze) (* 1/2 (+ z1 z2) sze)))))
 
 #_(defsketch example                  ;;Define a new sketch named example
   :title "Oh so many grey circles"  ;;Set the title of the sketch
   :setup setup                      ;;Specify the setup fn
-  :draw draw                        ;;Specify the draw fn
-  :size [323 200])                  ;
+  :draw #'draw3d                        ;;Specify the draw fn
+  :renderer :p3d
+  :size [1000 600])                  ;
 
 
 (assert (and (= (score-order conf2) 22)
